427bd538b8db0c0b617978068ab8265e8758dbb8
Nadin24 <jnadiusha@gmail.com>
2016-03-09 18:05:49 +0200

    Initial commit

---
+++ dz.php
@@ -0,0 +1,19 @@
+<?php
+$x =25;
+while($x<10)
+{
+    $x--;
+}
+echo($x);
+
+echo '<br>';
+
+echo (int) ((0.1 + 0.7) * 10);
+
+echo '<br>';
+
+switch(1) {
+    case 1: print("Книга");
+    case 2: print("Автор");
+    default: print("Книга не найдена");
+    }
